372fbad6e1132bbf611c0bc13a9b8bfddbbb2f7b
Manish <manish.perumalla@cgi.com>
2019-07-30 09:31:05 +0530

    Task5 Added

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,25 +1,17 @@
 package com.stackroute;
-import com.stackroute.domain.Actor;
+import com.stackroute.demo.BeanLifecycleDemoBean;
 import com.stackroute.domain.ConfigFile;
-import com.stackroute.domain.Movie;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import java.util.jar.JarOutputStream;
+
 public class Main
 {
     public static void main( String[] args )
     {
         AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(ConfigFile.class);
-        Movie movie1 =context.getBean("movie1",Movie.class);
-        Movie movie2 =context.getBean("movie2",Movie.class);
-        Actor actor1=context.getBean("actor1",Actor.class);
-        Actor actor2=context.getBean("actor2",Actor.class);
-        Actor actor3=context.getBean("actor3",Actor.class);
-        System.out.println(actor1.toString());
-        System.out.println(actor2.toString());
-        System.out.println(actor3.toString());
-        System.out.println(movie1.toString());
-        System.out.println(movie2.toString());
+        BeanLifecycleDemoBean demoBean=context.getBean("demobean",BeanLifecycleDemoBean.class);
+        demoBean.customDestroy();
+        demoBean.customInit();
+        demoBean.myMethod();
+        context.close();
     }
 }
--- src/main/java/com/stackroute/domain/ConfigFile.java
+++ src/main/java/com/stackroute/domain/ConfigFile.java
@@ -1,46 +1,17 @@
 package com.stackroute.domain;
-import com.stackroute.domain.Actor;
-import com.stackroute.domain.Movie;
 //import jdk.nashorn.internal.objects.annotations.Constructor;
+import com.stackroute.demo.BeanLifecycleDemoBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ConfigFile {
-    Actor actor1, actor2, actor3;
 
-    @Bean(name = "actor1")
-    public Actor getActor1() {
-        actor1 = new Actor("Manish", "male", 22);
-        return actor1;
+    @Bean(name="demobean")
+    public BeanLifecycleDemoBean etBeanLifeDemo(){
+        BeanLifecycleDemoBean beanLifecycleDemoBean = new BeanLifecycleDemoBean();
+
+        return beanLifecycleDemoBean;
     }
 
-    @Bean(name = "actor2")
-    public Actor getActor2() {
-        actor2 = new Actor("Krishna", "male", 22);
-        return actor1;
-    }
 
-    @Bean(name = "actor3")
-    public Actor getActor3() {
-        actor3 = new Actor("Spiderman", "female", 30);
-        return actor1;
-    }
-
-    @Bean(name = "movie1")
-    public Movie getMovie1() {
-        Movie movie1 = new Movie();
-        movie1.setActor1(actor1);
-        movie1.setActor2(actor2);
-        movie1.setActor3(actor3);
-        return movie1;
-    }
-
-    @Bean(name = "movie2")
-    public Movie getMovie2() {
-        Movie movie2 = new Movie();
-        movie2.setActor1(actor1);
-        movie2.setActor2(actor2);
-        movie2.setActor3(actor3);
-        return movie2;
-    }
 }
